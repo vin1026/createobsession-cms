d8872435f0af18f12d994cb39ce466dcf54cec0b
Vinh Vu <vvu1026@gmail.com>
2025-02-16 18:53:39 -0600

    Update server configuration for Render deployment

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 3000),
+  port: env.int('PORT', process.env.PORT || 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'http://localhost:3000'),
+  url: env('PUBLIC_URL', 'https://createobsession-cms.onrender.com'),
 });
